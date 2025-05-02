e6f97ff554c43be1aacac0a0e91106673b74aa3e
Afganirwansyahhidayat <afganirwansyah070708@gmail.com>
2025-05-02 07:08:29 +0700

    feat: add landingPage components & Logo website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import React from 'react'
 import './App.css'
+import LandingPages from './pages/landingPages'
 
 function App() {
   
 
   return (
     <>
-     
+      <LandingPages />
     </>
   )
 }
---
+++ src/pages/landingPages.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/landingPage/navbar";
+
+const LandingPages = () => {
+    return (
+        <>
+        <Navbar />
+        </>
+    )
+}
+
+export default LandingPages;
