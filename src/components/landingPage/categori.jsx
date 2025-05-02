d0ef2b2a746cbd92e59aefe54a88c7f8c4634a28
Afganirwansyahhidayat <afganirwansyah070708@gmail.com>
2025-05-02 23:28:58 +0700

    feat: add catagerios products

---
+++ src/components/landingPage/categori.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Categori = () => {
+    return (
+        <>
+        {/* categori */}
+           <div className="overflow-x-auto">
+  <div className="flex font-[poppins] font-semibold gap-7 items-center justify-center 
+                  bg-gray-100 h-[50px] cursor-pointer text-[12px] sm:text-[14px] md:text-[16px] lg:text-[20px] 
+                  text-black px-4 min-w-max whitespace-nowrap">
+    {["Brands", "New", "Men", "Woman", "Kids", "Sneaker", "Shoes", "Apparel", "Accessories"].map((item) => (
+      <div key={item} className="relative group">
+        <h1 className="transition duration-300 group-hover:text-gray-700">
+          {item}
+        </h1>
+        {/* underline effect */}
+        <span className="absolute left-0 -bottom-2 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
+      </div>
+    ))}
+  </div>
+</div>
+
+
+        </>
+    )
+}
+
+export default Categori;
--- src/pages/landingPages.jsx
+++ src/pages/landingPages.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/landingPage/navbar";
+import Categori from "../components/landingPage/categori";
 
 const LandingPages = () => {
     return (
         <>
         <Navbar />
+        <Categori />
         </>
     )
 }
